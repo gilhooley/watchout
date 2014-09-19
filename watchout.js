68a6877d411eb3dcfeea1ba380eae4a3bc23325e
gilhooley <katherine.gilhooley@hackreactor.com>
2014-09-19 14:32:06 -0700

    draw enemies in an svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,19 @@
-// start slingin' some d3 here.
 
+
+var gameboard = d3.select('.container')
+  .append('svg').attr('width',450).attr('height', 450)
+  .style('background','black');
+
+var dataCircle = [
+  {"x_axis":100,"y_axis":100,"radius":10,"color":"#fff"},
+  {"x_axis":150,"y_axis":150,"radius":10,"color":"#fff"}
+];
+
+var enemies = gameboard.selectAll('circle').data(dataCircle)
+  .enter().append('circle')
+  .attr('cx', function(d) { return d.x_axis; })
+  .attr('cy', function(d) { return d.y_axis; })
+  .attr('r', function(d) { return d.radius; })
+  .style('fill', function(d) {
+    return d.color;
+  });
